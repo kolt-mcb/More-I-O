68e4d66b6c23d0b413ea984f9d6ee6b38ef6b8a2
kolt mcbride <kolt.mcbride@lunarlogic.com>
2017-11-11 13:44:54 -0800

    ....

--- lib/neat.py
+++ lib/neat.py
@@ -136,7 +136,7 @@ class pool: #holds all species data, crossspecies settings and the current gene 
 			if not foundSpecies:
 				specie = self.newSpecies(self.Inputs,self.Outputs,self.recurrent)
 				child.defining = True
-				print(self.generations[generation][len(self.species)],len(self.species))
+				print(self.generations[self.generation][len(self.species)],len(self.species))
 				child.ID = {
 					"generation" : self.generation,
 					"specie"	 : len(self.species),
