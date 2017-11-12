92b8bdae7f87f23d5c986b6411068bfa50baea63
kolt mcbride <kolt.mcbride@lunarlogic.com>
2017-11-11 17:00:47 -0800

    ....

--- lib/neat.py
+++ lib/neat.py
@@ -100,7 +100,7 @@ class pool: #holds all species data, crossspecies settings and the current gene 
 
 	def addToPool(self,children):
 		for child in children:
-			relatives = self.getRelatives(child 
+			relatives = self.getRelatives(child)
 			if relatives == None:
 				child.relatives = []
 			foundSpecies = False
