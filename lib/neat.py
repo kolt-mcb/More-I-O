26edcfd9cf50624b63c90b1a1a4693488b93df61
kolt mcbride <koltafrickenfer@gmail.com>
2017-11-26 10:30:14 -0800

    mongo things

--- lib/neat.py
+++ lib/neat.py
@@ -152,7 +152,7 @@ class pool: #holds all species data, crossspecies settings and the current gene 
 			if self.client != None:
 				doc = self.getIDBSON(child.ID)
 				doc["game"] = self.timeStamp
-				doc = {**doc,**child.parents}
+				doc = {**doc,**self.getParentBSON(child.parents)}
 				self.updateMongoGenerations(doc)
 				self.updateMongoGenome(child)
 			after = time.time()
