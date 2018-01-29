c2dcbce4c87dfe55a946cd5d583d042d54321fc3
unknown <koltafrickenfer@gmail.com>
2018-01-28 21:41:47 -0800

    multiprocessing refactor maybe bug fix?

--- mario.py
+++ mario.py
@@ -229,64 +229,64 @@ class workerClass(object):
                         self.running.value = False
                     pass
                 else:
-                     job = self.jobs.get()
-                currentSpecies = job[0]
-                currentGenome = job[1]
-                genome = job[2]
-                maxDistance = 0
-                distance = None
-                staleness = 0
-                scores = []
-                finalScore = 0
-                done = False
-                maxReward = 0
-                for LVint in range(1):
-                    genome.generateNetwork()
+                    job = self.jobs.get()
+                    currentSpecies = job[0]
+                    currentGenome = job[1]
+                    genome = job[2]
                     maxDistance = 0
-                    oldDistance = 0
-                    bonus = 0
-                    bonusOffset = 0
+                    distance = None
                     staleness = 0
+                    scores = []
+                    finalScore = 0
                     done = False
-                    env.change_level(new_level=LVint)
-                    while not done:
-                        ob = env.tiles.flatten()
-                        o = genome.evaluateNetwork(ob.tolist(), discrete=False)
-                        o = joystick(o)
-                        ob, reward, done, _ = env.step(o)
-                        if 'ignore' in _:
-                            done = False
-                            env = gym.make('meta-SuperMarioBros-Tiles-v0')
-                            env.lock.acquire()
-                            env.reset()
-                            env.locked_levels = [False] * 32
-                            env.change_level(new_level=LVint)
-                            env.lock.release()
-                        distance = env._get_info()["distance"]
-                        if oldDistance - distance < -100:
-                            bonus = maxDistance
-                            bonusOffset = distance
-                        if maxDistance - distance > 50 and distance != 0:
-                            maxDistance = distance
-                        if distance > maxDistance:
-                            maxDistance = distance
-                            staleness = 0
-                        if maxDistance >= distance:
-                            staleness += 1
+                    maxReward = 0
+                    for LVint in range(1):
+                        genome.generateNetwork()
+                        maxDistance = 0
+                        oldDistance = 0
+                        bonus = 0
+                        bonusOffset = 0
+                        staleness = 0
+                        done = False
+                        env.change_level(new_level=LVint)
+                        while not done:
+                            ob = env.tiles.flatten()
+                            o = genome.evaluateNetwork(ob.tolist(), discrete=False)
+                            o = joystick(o)
+                            ob, reward, done, _ = env.step(o)
+                            if 'ignore' in _:
+                                done = False
+                                env = gym.make('meta-SuperMarioBros-Tiles-v0')
+                                env.lock.acquire()
+                                env.reset()
+                                env.locked_levels = [False] * 32
+                                env.change_level(new_level=LVint)
+                                env.lock.release()
+                            distance = env._get_info()["distance"]
+                            if oldDistance - distance < -100:
+                                bonus = maxDistance
+                                bonusOffset = distance
+                            if maxDistance - distance > 50 and distance != 0:
+                                maxDistance = distance
+                            if distance > maxDistance:
+                                maxDistance = distance
+                                staleness = 0
+                            if maxDistance >= distance:
+                                staleness += 1
 
-                        if staleness > 80 or done:
-                            scores.append(maxDistance - bonusOffset + bonus)
-                            if not done:
-                                done = True
-                        oldDistance = distance
-                for score in scores:
-                    finalScore += score
-                finalScore = round(finalScore / 32)
-                if self.running:
-                    self.results.put((finalScore, job))
+                            if staleness > 80 or done:
+                                scores.append(maxDistance - bonusOffset + bonus)
+                                if not done:
+                                    done = True
+                            oldDistance = distance
+                    for score in scores:
+                        finalScore += score
+                    finalScore = round(finalScore / 32)
+                    if self.running:
+                        self.results.put((finalScore, job))
 
-                print("species:", currentSpecies, "genome:",
-                    currentGenome, "Scored:", finalScore)
+                    print("species:", currentSpecies, "genome:",
+                        currentGenome, "Scored:", finalScore)
             time.sleep(1)
         
             
