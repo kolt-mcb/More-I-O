e69cb7d9a65567840906e22acf88c90376faa051
unknown <koltafrickenfer@gmail.com>
2018-01-28 20:52:27 -0800

    multiprocessing refactor maybe bug fix?

--- mario.py
+++ mario.py
@@ -430,13 +430,7 @@ class gui:
         if not self.resultQueue.empty():
             msg = self.resultQueue.get()
             if msg is not self.sentinel:
-                s = 0
-                for specie in self.pool.species:  # creates a job with species and genome index, env name and number of trials/attemps
-                    g = 0
-                    for genome in specie.genomes:
-                        print(s,g)
-                        g += 1
-                    s += 1
+
                 self.updateFitness(msg)
                 self.pool.nextGeneration()
                 playBest(self.pool.getBest())
@@ -507,11 +501,18 @@ class gui:
 
     def updateFitness(self,jobs):
         pool = ThreadPool(1)
-        pool.map(self.updateFitnessjob, jobs)
+        pool.map(self.updateFitnessjob,jobs)
 
     def updateFitnessjob(self,job):
         currentSpecies = job[1][0]
         currentGenome = job[1][1]
+        s = 0
+            for specie in self.pool.species:  # creates a job with species and genome index, env name and number of trials/attemps
+                g = 0
+                for genome in specie.genomes:
+                    print(s,g)
+                    g += 1
+                s += 1
         print(job)
         print(self.pool.species[currentSpecies].genomes)
         self.pool.species[currentSpecies].genomes[currentGenome].setFitness(job[0])
