5516660abbdd007c8e2928c5acbb4ccf4930e4e1
unknown <koltafrickenfer@gmail.com>
2018-02-02 20:07:26 -0800

    this refactor is taking a while

--- mario.py
+++ mario.py
@@ -238,10 +238,11 @@ class workerClass(object):
         results = []
         while self.initialized.value:
             print("wtf",len(results))
-            while len(results) != self.pool.Popluation:
+            while len(results) != self.numJobs:
+                print(len)
                 if not self.results.empty():
                     results.append(self.results.get())
-            if len(results) == self.pool.Popluation:
+            if len(results) == self.numJobs:
                 self.updateFitness(results)
                 print(self.pool.generation)
                 self.pool.nextGeneration()
