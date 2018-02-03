eae9b73ffad731f5e06d7434a2c3aa6fa70d8405
unknown <koltafrickenfer@gmail.com>
2018-02-02 18:37:44 -0800

    this refactor is taking a while

--- mario.py
+++ mario.py
@@ -465,9 +465,9 @@ class gui:
                 self.master.after(250,lambda: self.checkRunCompleted(pausing))
             else:
                 sharedRunning.value = True
-            self.master.after(250,self.checkRunCompleted)
+            self.master.after(.5,self.checkRunCompleted)
         else:
-            self.master.after(250, self.checkRunPaused)
+            self.master.after(.5, self.checkRunPaused)
 
 
     
