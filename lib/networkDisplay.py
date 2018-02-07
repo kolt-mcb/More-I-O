895efdecdafbb278304b105a9ff7bf83641feae5
unknown <koltafrickenfer@gmail.com>
2018-02-06 22:33:16 -0800

    this refactor is taking a while

--- lib/networkDisplay.py
+++ lib/networkDisplay.py
@@ -213,8 +213,8 @@ class newNetworkDisplay():
                 self.drawnLines[gene.innovation] = self.canvas.create_line(c1.x-(c1.gridSize//2), c1.y-(c1.gridSize//2), c2.x-(c2.gridSize//2), c2.y-(c2.gridSize//2),fill=color,width=3)
 
     def updateCanvas(self,genome):
-        if self.lastGenome.ID != genome.ID:
-            self.lastGenome= genome
+        if self.lastGenome != genome.ID:
+            self.lastGenome= genome.ID
             self.update(genome)
         self.updateCells(genome)
         self.updateLines(genome)
